8811cbfccd5aa7792655027d0372e3da0bbc9b48
Didem Peker <pek240759@spengergasse.at>
2024-10-03 11:32:03 +0200

    Initial sharing of project

---
+++ Student.java
@@ -0,0 +1,89 @@
+public class Student 
+{
+    private String  name;           // Name des Studierenden
+    private int     alter;          // Alter des Studierenden
+    private String  religion;       // ...
+    private boolean matura;
+    
+    // Constructor
+    public Student(String neuName, int neuAlter, String neuReligion, boolean neuMatura)
+    {
+        setName(neuName);
+        setAlter(neuAlter);
+        setReligion(neuReligion);
+        setMatura(neuMatura);
+    }
+    
+    public Student(String neuName,int neuAlter, String neuReligion)
+    {
+        setName(neuName);
+        setAlter(neuAlter);
+        setReligion(neuReligion);
+        setMatura(false);
+    }
+    
+    public Student(String neuName)
+    {
+        setName(neuName);
+        setAlter(14);
+        setReligion("ob");
+        setMatura(false);       
+    }
+    
+    public Student()
+    {
+        setName("UNKN");
+        setAlter(14);
+        setReligion("ob");
+        setMatura(false);
+    }
+    
+    public void setName(String neuName)
+    {
+        /*
+         * Die Eigenschaft name wird überschrieben mit dem
+         * Wert des Parameters.
+         */
+        name = neuName;
+    }
+
+    public void setAlter(int neuAlter)
+    {
+        alter = neuAlter;
+    }
+
+    public void setReligion(String neuReligion)
+    {
+        religion = neuReligion;
+    }
+    
+    public void setMatura(boolean neuMatura)
+    {
+        matura = neuMatura;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getAlter()
+    {
+        return alter;
+    }
+
+    public String getReligion()
+    {
+        return religion;
+    }
+    
+    public boolean getMatura()
+    {
+        return matura;
+    }
+    
+    public void printStudent()
+    {
+        System.out.println(name + ": " + alter + " Jahre, Rel: " + religion + ", Matura: " + matura);
+    }
+}
